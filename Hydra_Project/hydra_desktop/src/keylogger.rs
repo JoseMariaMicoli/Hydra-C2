ef4d91beb98b23940bba9f5c4127b58f460705f2
Xoce <josemaria.micoli@gmail.com>
2026-02-17 12:23:19 -0300

    Update LICENSE and source/docs headers

--- Hydra_Project/hydra_desktop/src/keylogger.rs
+++ Hydra_Project/hydra_desktop/src/keylogger.rs
@@ -1,3 +1,18 @@
+
+/*
+Copyright (c) 2026 José María Micoli
+Licensed under AGPLv3
+
+You may:
+✔ Study
+✔ Modify
+✔ Use for internal security testing
+
+You may NOT:
+✘ Offer as a commercial service
+✘ Sell derived competing products
+*/
+
 use rdev::{listen, EventType};
 use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
 use std::thread;
--- genesis.sh
+++ genesis.sh
@@ -1,3 +1,18 @@
+
+/*
+Copyright (c) 2026 José María Micoli
+Licensed under AGPLv3
+
+You may:
+✔ Study
+✔ Modify
+✔ Use for internal security testing
+
+You may NOT:
+✘ Offer as a commercial service
+✘ Sell derived competing products
+*/
+
 #!/bin/bash
 
 # 1. Clean up previous attempts
